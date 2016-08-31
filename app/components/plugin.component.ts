e1ba9364a996b1c62a60a8c93ebd5204e378fb15
vherever <vherever@gmail.com>
2016-08-31 15:30:16 +0300

    initial setup

---
+++ app/components/plugin.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+@Component({
+    selector: 'it7-availabilities-submission-plugin',
+    template: `<h1>Availabilities Submission Plugin</h1>`
+})
+export class PluginComponent {
+
+}
---
+++ app/main.ts
@@ -0,0 +1,5 @@
+///<reference path="../typings/index.d.ts"/>
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import {PluginComponent} from "./components/plugin.component";
+
+bootstrap(PluginComponent);
