12034510259d78ecbbfa6f02c6ecfed4369df9d5
vherever <vherever@gmail.com>
2016-09-01 18:50:58 +0300

    generate date from mock ajax service

--- app/components/my-availabilities-list.component.ts
+++ app/components/my-availabilities-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
 import {MyAvailabilitiesListItem} from "./my-availabilities-list-item.component";
 import {Availability} from "../models/availabilities";
 import {AvailabilitiesService} from "../services/availabilities.service";
@@ -10,30 +10,17 @@ import {AvailabilitiesService} from "../services/availabilities.service";
     ]
 })
 export class MyAvailabilitiesList {
-    availabilities: Availability[] = [
-        {
-            'id': 1,
-            'date': '01.02.2016',
-            'time': 'Morning',
-            'status': 'Available',
-            'description': 'some comment',
-            'isDeletable': true
-        },
-        {
-            'id': 2,
-            'date': '02.02.2016',
-            'time': 'Evening',
-            'status': 'Confirmed',
-            'description': '',
-            'isDeletable': false
-        }
-    ];
+    @Input() list: Availability[];
 
+    constructor(private availabilities: AvailabilitiesService) {
+    }
 
-    constructor(
-        // private availabilityService: AvailabilitiesService
-    ) {
-        console.log('availabilities', this.availabilities);
-        // this.availabilities = availabilityService.getAvailabilities();
+    ngOnInit() {
+        this.availabilities.onUpdate.subscribe(availabilities => this.updateList(availabilities));
+    }
+
+    // Call from this class closure
+    updateList(availabilities: Availability[]){
+        this.list = availabilities;
     }
 }
